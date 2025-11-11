661576057246bf9a44d7155bcd0783fb8d51bac7
jochris <chris.johan.225@gmail.com>
2025-11-11 13:17:59 +0100

    ajout du formulaire de connexion admin

---
+++ app/admin/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
