4afb06f01935f10c323266f907f291ef8eaf0eea
jochris <chris.johan.225@gmail.com>
2025-11-14 14:15:19 +0100

    premier interface newsletters

--- app/admin/Dashboard/newsletters/page.tsx
+++ app/admin/Dashboard/newsletters/page.tsx
@@ -1,5 +1,46 @@
-export default function Newsletters(){
-    return (
-    <div><h1>Newsletters</h1> </div>
+import React from 'react'
+import ComponenteFormulaire from './components/ComponenteFormulaire';
+import Affichage from './components/Affichage';
+
+export default function page() {
+  const container: React.CSSProperties = {
+      backgroundColor: '#6B94AD',
+      minHeight: '100vh',
+      padding: '40px',
+      fontFamily: 'Arial, sans-serif',
+      display: 'flex',
+      gap: '30px'
+      
+    };
+    const leftSection: React.CSSProperties = {
+        flex: '1',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '30px',
+    
+      };
+    
+      const rightSection: React.CSSProperties = {
+        width: '350px',
+        height: 'fit-content',
+        alignItems:'flex-start',
+        padding:"27px"
+      };
+      const title: React.CSSProperties = {
+          color: 'white',
+          fontSize: '36px',
+          fontWeight: 'bold',
+          marginBottom: '30px'
+        };
+  return (
+    <div style={container}>
+      <div style={leftSection}>
+        <h1 style={title}>Gestion des Newsletters</h1>
+        <Affichage/>
+      </div>
+      <div style={rightSection}>
+        <ComponenteFormulaire/>
+      </div>
+    </div>
   )
 }
