9bc6d3618537cb16adbfe64c79f93d844e91b59a
jochris <chris.johan.225@gmail.com>
2025-11-18 09:42:16 +0100

    bonjour bayo

--- app/admin/Dashboard/formations_bourses/page.tsx
+++ app/admin/Dashboard/formations_bourses/page.tsx
@@ -1,5 +1,9 @@
-export default function FormationBourse(){
-    return (
-    <div><h1>FormationBourse</h1> </div>
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="bg-red-500 p-4 text-white">
+      <h1>bayo</h1>
+    </div>
   )
 }
