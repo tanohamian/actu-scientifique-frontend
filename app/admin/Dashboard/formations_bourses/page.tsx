3527bde84492cafd27780a6da1c635908c3bb6e9
jochris <chris.johan.225@gmail.com>
2025-11-18 11:39:32 +0100

    ajout du dernier UI sans responsive

--- app/admin/Dashboard/formations_bourses/page.tsx
+++ app/admin/Dashboard/formations_bourses/page.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import SwitchSection from './components/Section';
+
+  const container: React.CSSProperties = {
+        minHeight: '100vh',
+        padding: '40px',
+        fontFamily: 'Arial, sans-serif',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '30px',
+      };
+
+  const entete : React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+
+  const title: React.CSSProperties = {
+            color: 'white',
+            fontSize: '36px',
+            fontWeight: 'bold',
+            marginBottom: '1px'
+          };
+
+  const soustitre: React.CSSProperties = {
+            color: 'white',
+            fontSize: '20px',
+            fontWeight: 'bold',
+            marginBottom: '30px'
+          };
 
 export default function page() {
   return (
-    <div className="bg-red-500 p-4 text-white">
-      <h1>bayo</h1>
+    <div style={container}>
+      <div style={entete}>
+        <h1 style={title}> Gestion de contenu </h1>
+        <h2 style={soustitre}> Gérer les bourses, les formations et les jeux </h2>
+      </div>
+      <SwitchSection/>
     </div>
   )
 }
