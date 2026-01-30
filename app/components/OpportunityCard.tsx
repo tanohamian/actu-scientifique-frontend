caf8b7179e2f7431d873fbd1b02e605521d36300
BenRango <srangoelie@gmail.com>
2026-01-30 15:55:34 +0000

    push

--- app/components/OpportunityCard.tsx
+++ app/components/OpportunityCard.tsx
@@ -10,8 +10,8 @@ interface OpportunityCardProps {
 export default function OpportunityCard({isScholarship, content, url}: OpportunityCardProps){
 
     return(
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            <article className="rounded-bl-none shadow-xl w-120 max-w-full bg-[#AEB8C1] overflow-y-auto text-black font-bold p-5">
+        <a href={url} target="_blank" rel="noopener noreferrer" className='w-fit'>
+            <article className="rounded-lg shadow-xl w-120 max-w-full bg-[#AEB8C150] overflow-y-auto text-white font-bold p-5">
                 {isScholarship?  <FontAwesomeIcon icon={faGraduationCap}/> : <FontAwesomeIcon icon={faBookOpen}/>}
                 <p>{content}</p>
             </article>
