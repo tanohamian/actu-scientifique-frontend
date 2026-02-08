52604f18301724e0837b76c0028e9379fbcec37e
BenRango <srangoelie@gmail.com>
2026-02-08 22:20:52 +0000

    chore: corrected baseUrl

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,4 +1,4 @@
 export const env = {
     onProduction : process.env.CUSTOM_NODE_ENV === "production" as string ? true: false,
-    baseUrl : process.env.API_MODE === "production" as string ? process.env.LOCAL_BASE_URL as string : "https://api.actuscientifique.com/actu-sciences",
+    baseUrl : process.env.API_MODE === "production" as string ? process.env.BASE_URL as string : process.env.LOCAL_BASE_URL as string
 } as const;
