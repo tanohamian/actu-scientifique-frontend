d5afda86579a91dfef726a61740f76452faa15b8
BenRango <srangoelie@gmail.com>
2026-02-08 20:57:51 +0000

    chande production condition

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-    production : process.env.API_MODE === "production" as string ? true: false,
+    production : process.env.NODE_ENV === "production" as string ? true: false,
     baseUrl : process.env.API_MODE === "production" as string ? process.env.LOCAL_BASE_URL as string : "https://api.actuscientifique.com/actu-sciences",
 } as const;
