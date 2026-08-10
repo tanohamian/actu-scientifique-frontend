e5e9bb322f11d86d2a38b0889a7e8f9e074c19a3
Tanoh <jeanamian2@gmail.com>
2026-08-10 15:11:42 +0000

    feat:remove language switcher in admin side

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,23 +1,22 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import { notFound } from 'next/navigation';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import CustomizedSwitches from "./components/switchLanguage";
 
 export default async function RootLayout({
   children,
-  params
+  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ lang: string }>
+  params: Promise<{ lang: string }>;
 }) {
-
   const { lang } = await params;
 
-  if (!['fr', 'en'].includes(lang)) {
+  if (!["fr", "en"].includes(lang)) {
     notFound();
   }
 
@@ -26,15 +25,29 @@ export default async function RootLayout({
   return (
     <html lang={lang} translate="no">
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.svg" />
-        <title>{lang === 'fr' ? "L'actualité scientifique" : "Science news"}</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon.svg"
+        />
+        <title>
+          {lang === "fr" ? "L'actualité scientifique" : "Science news"}
+        </title>
         <meta name="google-adsense-account" content="ca-pub-7800085793195104" />
       </head>
       <body>
-        <AppRouterCacheProvider options={{ key: 'mui' }}>
+        <AppRouterCacheProvider options={{ key: "mui" }}>
           <NextIntlClientProvider messages={messages} locale={lang}>
-            <section style={{position : 'absolute', top: '10px', right : '10px', zIndex: 3}}>
-              <CustomizedSwitches></CustomizedSwitches>
+            <section
+              style={{
+                position: "absolute",
+                top: "10px",
+                right: "10px",
+                zIndex: 3,
+              }}
+            >
+              <CustomizedSwitches />
             </section>
             {children}
           </NextIntlClientProvider>
